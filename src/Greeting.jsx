1b6687b1efb4243aa33c320ec3534372b164d627
Kevin <thevoidkc@gmail.com>
2026-05-22 09:06:28 -0700

    feat: experiment with components

---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return <h1>"I swear by my pretty floral bonnet, I will end you."</h1>;
+}
+
+function Food() {
+  return <img src="/Cheeseburger.jpg" alt="Dog" />;
+}
+
+export { Greeting, Food };
